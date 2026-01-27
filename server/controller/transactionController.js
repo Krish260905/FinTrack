c03ed1ba22f477518eb2c92363966983bd4cc961
Krish260905 <krishjpatel26@gmail.com>
2026-01-27 22:18:57 +0530

    feat : addTtransaction

--- server/controller/transactionController.js
+++ server/controller/transactionController.js
@@ -39,6 +39,50 @@ export const getDashboardInformation = async (req, res) => {
 
 export const addTransaction = async (req, res) => {
     try {   
+
+        const { userId } = req.body.user;
+        const { account_id } = req.params;
+        const { amount, description, source } = req.body;
+
+        if(!amount || !description || !source) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+            
+
+        if (number(amount) <= 0) {
+            return res.status(400).json({ message: "Amount must be greater than zero" });
+        }
+
+         const result = await pool.query({
+            text : `SELECT * FROM tblaccount WHERE id = $1 `,
+            values: [account_id]
+        });
+
+        const accountInfo = result.rows[0];
+
+        if (!accountInfo) {
+            return res.status(404).json({ message: "Account not found" });
+        }
+
+        if (accountInfo.account_balance <= 0 || accountInfo.account_balance < Number(amount)) {
+            return res.status(400).json({ message: "Insufficient account balance" });
+        }
+
+        await pool.query('BEGIN');
+           await pool.query({
+            text : `UPDATE tblaccount SET account_balance = account_balance - $1, updated_at = CURRENT_TIMESTAMP WHERE id = $2`,
+            values: [amount, account_id]
+        });
+
+        await pool.query({
+            text : `INSERT INTO tbltransaction (user_id, amount, description, source, status, type) VALUES ($1, $2, $3, $4, $5, $6)`,
+            values: [userId, amount, description, source, 'completed','expense']
+        });
+
+        await pool.query('COMMIT');
+
+        res.status(201).json({ message: "Transaction added successfully" });
+        
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
